35425fe45d5745d056c8c12fe8810e36d6008a32
Sayan Goel <sayangoel14@gmail.com>
2026-01-19 18:23:19 +0530

    Update backend authentication files

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import axios from "axios";
+import BASE_URL from "./config";
 
 import GeneralContext from "./GeneralContext";
 
